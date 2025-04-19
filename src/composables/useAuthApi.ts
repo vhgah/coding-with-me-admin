26d302901f0b901ba9e706abe821c11d20611335
HoangAnh <vhgah98@gmail.com>
2025-04-19 15:55:54 +0700

    update login route

--- src/composables/useAuthApi.ts
+++ src/composables/useAuthApi.ts
@@ -2,7 +2,7 @@ import httpClient from '@/api/httpClient'
 
 export default function useAuthApi () {
   const login = async (email: string, password: string) => {
-    return httpClient.post('/admin/login', {
+    return httpClient.post('/login', {
       email,
       password
     })
